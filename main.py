d5a5b988c22399464e111302279af4c9b73cf1a0
LemanRus <wlad163163@gmail.com>
2024-08-12 17:11:34 +0400

    Add subfolder

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import serial
 import datetime
@@ -9,11 +10,11 @@ cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 if not (cap.isOpened()):
     print("Could not open video device")
 else:
-    print("Camera started succesfully")
+    print("Camera started successfully")
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-cap.set(cv2.CAP_PROP_FOCUS, 255)
+cap.set(cv2.CAP_PROP_FOCUS, 30)
 
 try:
     s = serial.Serial('COM4', baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
@@ -21,10 +22,17 @@ except serial.serialutil.SerialException:
     print("Can't open COM port, check and restart")
     sys.exit()
 
+folder_name = "Started at " + datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+os.makedirs(folder_name)
+os.chdir(folder_name)
+
+img_counter = 1
+
 while True:
     command = s.readline().decode("Ascii")
     if command.strip() == "SMILE":
         ret, frame = cap.read()
-        img_name = datetime.datetime.now().strftime("%Y_%m_%d__%H_%M_%S") + ".jpg"
+        img_name = f"{img_counter}.jpg"
         cv2.imwrite(img_name, frame)
-        print("{} is captured".format(img_name))
+        print(f"{img_name} is captured")
+        img_counter += 1
